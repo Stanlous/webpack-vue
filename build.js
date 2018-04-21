a4ee0f497cfbbc4fc2095ba4cfded337b430810e
Stanlous <ystanlous@gmail.com>
2018-04-22 01:38:13 +0800

    open window after compilation

--- build.js
+++ build.js
@@ -1,17 +1,9 @@
-const express = require('express')
 const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-
 const IS_PRODUCTION = process.env.NODE_ENV == 'production'
-
 console.log('IS_PRODUCTION:', IS_PRODUCTION)
 
-const app = express();
-const devConfig = require('./config/webpack/webpack.dev.js')
 const prodConfig = require('./config/webpack/webpack.prod.js')
-const config = IS_PRODUCTION ? prodConfig : devConfig
-const compiler = webpack(config)
+const compiler = webpack(prodConfig)
 
 if (IS_PRODUCTION) {
   return compiler.run((err, stats) => {
@@ -31,23 +23,6 @@ if (IS_PRODUCTION) {
 
   })
 }
-
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  quiet: true,
-  writeToDisk: (filepath) => {
-    console.log(filepath)
-    return false
-    return !/\.json$/.test(filepath)
-  }
-}))
-
-app.use(webpackHotMiddleware(compiler))
-
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!\n');
-});
-
 
 function fail(err) {
   // FriendlyErrorsWebpackPlugin has handled and logged errors,
--- server/app.js
+++ server/app.js
@@ -4,10 +4,21 @@ const webpacHotMiddleware = require('koa-webpack-hot-middleware')
 const IS_PRODUCTION = process.env.NODE_ENV == 'production'
 const devConfig = require('../config/webpack/webpack.dev.js')
 const compiler = webpack(devConfig)
+const open = require('opn')
+
 
 module.exports = app => {
   if (!IS_PRODUCTION) {
     app.beforeStart(async () => {
+      let opened = false
+      compiler.hooks.done.tapAsync('nothing', (compiler, callback) => {
+        if (opened) {
+          return callback()
+        }
+        opened = true
+        open(`http://localhost:${app.config.cluster.listen.port}`)
+        callback()
+      })
       app.use(webpackDevMiddleware(compiler, {
         publicPath: devConfig.output.publicPath
       }))
